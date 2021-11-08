a00a96c598d0be749d22be71912db110827d94f5
bobthedev90368 <84109060+bobthedev90368@users.noreply.github.com>
2021-11-08 16:35:19 -0500

    Update RUNNER.js

--- RUNNER.js
+++ RUNNER.js
@@ -1,1 +1,13 @@
+const noblox = require('noblox.js')
+const cookie = process.env['cookie']
+async function startApp () {
+    // You MUST call setCookie() before using any authenticated methods [marked by 🔐]
+    // Replace the parameter in setCookie() with your .ROBLOSECURITY cookie.
+    const currentUser = await noblox.setCookie('')
+    console.log(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`)
 
+    // Do everything else, calling functions and the like.
+    const groupInfo = await noblox.getGroup(9997719)
+    console.log(groupInfo)
+}
+startApp()
